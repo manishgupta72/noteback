cfc37c154cc6e78803ca789fc70f47c211a416f8
Manish Gupta <manishgupta724872@gmail.com>
2023-10-09 19:37:14 +0530

    initial

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 const connectMongo = require('./db')
 const dotenv=require('dotenv')
+const cors=require('cors')
 const express = require('express')
 connectMongo();
 
+dotenv.config({path:'./config.env'})
 const app = express();
 const PORT = process.env.PORT;
 
-dotenv.config({path:'./config.env'})
+
 //The express. json() function is a middleware function used in Express.
 // js applications to parse incoming JSON data from HTTP requests,
 // a standard format for data transmission in web servers.
 app.use(express.json())
-
+app.use(cors())
 //Available routes
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/notes', require('./routes/notes'))
